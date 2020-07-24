f0270037fb8bee7273020d0d8101b8aeb31b461e
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-07-24 11:20:26 +0200

    test for mail

--- functions.php
+++ functions.php
@@ -209,24 +209,38 @@ require 'inc/geotraces-acf-block.php';
 /////////
 // Rewrite reset password mail
 ////////
-add_filter( 'retrieve_password_message', 'sedoo_geotr_replace_reset_pass_mail', 10, 4 );
-function sedoo_geotr_replace_reset_pass_mail( $message, $key, $user_login, $user_data ) {
+//* Password reset activation E-mail -> Body
+add_filter( 'retrieve_password_message', 'sedoo_geotraces_retrieve_password_message', 10, 2 );
+function sedoo_geotraces_retrieve_password_message( $message, $key ){
+    $user_data = '';
+    // If no value is posted, return false
+    if( ! isset( $_POST['user_login'] )  ){
+            return '';
+    }
+    // Fetch user information from user_login
+    if ( strpos( $_POST['user_login'], '@' ) ) {
 
-    // Start with the default content.
-    $site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
-    $message = __( 'Someone has requested a password reset for the following account:' ) . "\r\n\r\n";
-    /* translators: %s: site name */
-    $message .= sprintf( __( 'Site Name: %s' ), $site_name ) . "\r\n\r\n";
-    /* translators: %s: user login */
-    $message .= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
-    $message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' ) . "\r\n\r\n";
-    $message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
-    $message .= '<' . network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . ">\r\n";
-
-    
-    // Return the filtered message.
+        $user_data = get_user_by( 'email', trim( $_POST['user_login'] ) );
+    } else {
+        $login = trim($_POST['user_login']);
+        $user_data = get_user_by('login', $login);
+    }
+    if( ! $user_data  ){
+        return '';
+    }
+    $user_login = $user_data->user_login;
+    $user_email = $user_data->user_email;
+    // Setting up message for retrieve password
+    $message = "Looks like you want to reset your password!\n\n";
+    $message .= "Please click on this link:\n";
+    $message .= '<a href="';
+    $message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login');
+    $message .= '">"';
+    $message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login');
+    $message .= '"</a>\n\n"';
+    $message .= 'Kind Regards,<br/>Dream Team';
+    // Return completed message for retrieve password
     return $message;
-
 }
 
 ?>
