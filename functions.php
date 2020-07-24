d872de00441698f8c5066d42b50a6e29a4d9937d
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-07-24 11:13:48 +0200

    change reset password mail test

--- functions.php
+++ functions.php
@@ -204,4 +204,29 @@ add_filter( 'login_redirect', 'login_redirect', 10, 3 );
  */
 require 'inc/geotraces-acf-config.php';
 require 'inc/geotraces-acf-block.php';
+
+
+/////////
+// Rewrite reset password mail
+////////
+add_filter( 'retrieve_password_message', 'sedoo_geotr_replace_reset_pass_mail', 10, 4 );
+function sedoo_geotr_replace_reset_pass_mail( $message, $key, $user_login, $user_data ) {
+
+    // Start with the default content.
+    $site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+    $message = __( 'Someone has requested a password reset for the following account:' ) . "\r\n\r\n";
+    /* translators: %s: site name */
+    $message .= sprintf( __( 'Site Name: %s' ), $site_name ) . "\r\n\r\n";
+    /* translators: %s: user login */
+    $message .= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
+    $message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' ) . "\r\n\r\n";
+    $message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
+    $message .= '<' . network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . ">\r\n";
+
+    
+    // Return the filtered message.
+    return $message;
+
+}
+
 ?>
